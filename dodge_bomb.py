e23ec211a8f4918f9fddb6fe53c730bf22d76bff
C0A23176 <c0a2317658@edu.teu.ac.jp>
2025-09-30 14:42:33 +0900

    課題4こうかとんと爆弾の当たり判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -48,7 +48,9 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
-
+        if kk_rct.colliderect(bb_rct):  # こうかとんと爆弾が重なったら
+            return  # ゲームオーバー
+        
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for key, mv in DELTA.items():
